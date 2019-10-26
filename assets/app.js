952f12f0bd9828e77ae5abdf99482669848961fc
Valentine <abraval@gmail.com>
2019-10-26 15:50:58 -0400

    reorganized app.js, added a function to display searches=d gifs

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,53 @@
 var feelings = ["Happy", "Angry", "Bored", "Excied", "Confused", "Annoyed", "Contempt", "Surprised", "Disgusted", "Scared", "Emotional", "Amused", "Joyful", "Romantic", "Sensitive"];
 
+
+function displayGif() {
+
+  var feeling = $(this).attr("data-name");
+  console.log(feeling)
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + feeling + "&api_key=u61R4WTo5dw995TP19npNy03UWNNeeJZ&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    .then(function (response) {
+      console.log(response)
+
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var feelingImage = $("<img>");
+
+          feelingImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.append(p);
+          gifDiv.append(feelingImage);
+
+          $("#gif-view").prepend(gifDiv);
+        }
+      }
+    });
+
+
+};
+
+
+
+
+
 function renderButtons() {
 
   $("#buttons-view").empty();
@@ -7,26 +55,32 @@ function renderButtons() {
   for (var i = 0; i < feelings.length; i++) {
 
     var a = $("<button>");
-    
-    a.addClass("feelings-btn btn-lg btn-info border-0 rounded shadow text-white m-1");
-  
+
+    a.addClass("btn-f btn-lg btn-info border-0 rounded shadow text-white m-1");
+
     a.attr("data-name", feelings[i]);
-   
+
     a.text(feelings[i]);
-  
+
     $("#buttons-view").append(a);
+    // console.log(a)
   }
 }
 
-$("#add-gif").on("click", function(event) {
+$("#add-gif").on("click", function (event) {
+
   event.preventDefault();
 
   var gif = $("#gif-input").val().trim();
 
   feelings.push(gif);
-  console.log(feelings)
 
   renderButtons();
 });
 renderButtons();
+
+$(document).on("click", ".btn-f", displayGif);
+
+// $(document).on("click", ".movie-btn", displayMovieInfo);
+
 
